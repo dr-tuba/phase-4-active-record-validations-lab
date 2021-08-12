55a79b13b61cb4508b439cc2960e7981e32829d3
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-08-11 18:04:39 -0600

    finished lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord    
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        unless titles.any? { |i| i.match(title) }
+            errors.add(:title, "Must be click bait title")
+        end
+    end
 end
